19ac44ed63e3af935890a094810b3f4a0db9117c
Gifteddy <114597670+Gifteddy@users.noreply.github.com>
2025-11-21 09:43:23 +0100

    Refactor Header component for improved styling

--- frontend/src/components/Layout/Header.jsx
+++ frontend/src/components/Layout/Header.jsx
@@ -54,8 +54,8 @@ const Header = ({ activeHeading }) => {
 
   return (
     <>
-      <div className={`${styles.section}`}>
-        <div className="hidden 800px:h-[50px] 800px:my-[20px] 800px:flex items-center justify-between">
+      <div className={`${styles.section} z-50`}>
+        <div className="hidden 800px:h-[50px] 800px:my-[20px] 800px:flex items-center justify-between z-50">
           <div>
           <Link to="/">
           {/* Use the imported logo */}
